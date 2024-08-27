a38ca0537e1e03dde305b2d34ed1fd933ec86818
Jonas Amundsen <jonasba@gmail.com>
2024-08-27 19:39:06 +0200

    Comment

--- cypress/Cypress.require/cypress.config.js
+++ cypress/Cypress.require/cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
   e2e: {
     video: false,
     setupNodeEvents(on, config) {
+      // Remove the following block (custom preprocessor) and the example runs.
       on(
         "file:preprocessor",
         createBundler({})
